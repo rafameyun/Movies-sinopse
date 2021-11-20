bc5c5e5353bbac2709d8854d1cbb927ce7accaba
rafameyun <raffinhah4@gmail.com>
2021-11-20 04:11:50 -0300

    Add API, KEY, IMGPATH and Document selector

---
+++ script.js
@@ -0,0 +1,7 @@
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3cf2747f302d6d636fc27e224c45e9b7&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=3cf2747f302d6d636fc27e224c45e9b7&query=";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
